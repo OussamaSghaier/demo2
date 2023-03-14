21bb0b3fdf36a73de3249901e4d62030c04d23ad
Oussama Ben Sghaier <oussama.ben.sghaier@umontreal.ca>
2023-03-14 17:51:14 -0400

    create gitignore file

--- Calculatrice.java
+++ Calculatrice.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Calculatrice{
     public static void main(String[] args){
 
-        System.out.println("*** Menu Calculatrice -- ***");
+        System.out.println("*** Menu Calculatrice ***");
+        System.out.println("*** Menu Calculatrice ***");
         Calculatrice calculatrice = new Calculatrice();
         calculatrice.displayMenu();
 
