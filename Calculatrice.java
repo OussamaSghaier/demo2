5bddd71cd5c3fa7b8399fa4f4620483e00741f8f
Oussama Ben Sghaier <oussama.ben.sghaier@umontreal.ca>
2023-03-14 17:02:09 -0400

    implementer addition

--- Calculatrice.java
+++ Calculatrice.java
@@ -2,4 +2,9 @@ public class Calculatrice{
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+    public int addition(int a, int b){
+        return a + b;
+    }
+
 }
